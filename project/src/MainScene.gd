8147c51896f9fec5b058bb2e68706d864cf15178
Paul Gestwicki <pvgestwicki@bsu.edu>
2021-01-29 10:17:43 -0500

    Adjust size and layout

--- project/src/MainScene.gd
+++ project/src/MainScene.gd
@@ -1,9 +1,14 @@
 extends Control
 
+enum State {
+	NONE, STUDY, DRAW
+}
 
-export var studyDuration := 5
+export var studyDuration := 20
+export var drawDuration := 120
 
 var _secondsRemaining : int = 0
+var _state = State.NONE
 
 onready var _startButton := $VBoxContainer/StartButton
 onready var _timer := $Timer
@@ -13,6 +18,7 @@ onready var _beepPlayer := $AudioStreamPlayer
 
 func _on_StartButton_pressed():
 	_startButton.disabled = true
+	_state = State.STUDY
 	_secondsRemaining = studyDuration
 	_timeRemainingLabel.text = _formatSecondsRemaining()
 	_timer.start(1)
@@ -23,8 +29,13 @@ func _on_Timer_timeout():
 	_timeRemainingLabel.text = _formatSecondsRemaining()	
 	if _secondsRemaining == 0:
 		_beepPlayer.play()
-		_startButton.disabled = false
-		_timer.stop()
+		match _state:
+			State.STUDY:
+				_secondsRemaining = drawDuration
+			State.DRAW:
+				_startButton.disabled = false
+				_state = State.NONE
+				_timer.stop()
 
 
 func _formatSecondsRemaining() -> String:
