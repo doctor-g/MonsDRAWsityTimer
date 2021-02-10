ad906ca52615b021b4efe4d2c577ff255c08acdb
Paul Gestwicki <pvgestwicki@bsu.edu>
2021-02-09 19:35:52 -0500

    Add pause button

--- project/src/MainScene.gd
+++ project/src/MainScene.gd
@@ -15,15 +15,18 @@ onready var _timer := $Timer
 onready var _timeRemainingLabel := $VBoxContainer/TimeRemaining
 onready var _beepPlayer := $AudioStreamPlayer
 onready var _cancelButton := $VBoxContainer/CancelButton
-
+onready var _pauseButton := $VBoxContainer/PauseButton
 
 func _on_StartButton_pressed():
 	_startButton.disabled = true
 	_cancelButton.disabled = false
+	_pauseButton.disabled = false
 	_state = State.STUDY
 	_secondsRemaining = studyDuration
 	_timeRemainingLabel.text = _formatSecondsRemaining()
 	_timer.start(1)
+	 # In case the timer was previously paused, make sure to unpause it.
+	_timer.paused = false
 
 
 func _on_Timer_timeout():
@@ -38,6 +41,7 @@ func _on_Timer_timeout():
 				_state = State.DRAW
 			State.DRAW:
 				_startButton.disabled = false
+				_pauseButton.disabled = true
 				_state = State.NONE
 				_cancelButton.disabled = true
 				_timer.stop()
@@ -58,4 +62,18 @@ func _on_CancelButton_pressed():
 	_startButton.disabled = false
 	_cancelButton.disabled = true
 	_timeRemainingLabel.text = _formatSecondsRemaining()
-	
+	_pauseButton.disabled = true
+	_stop_blink_animation()
+
+
+func _on_PauseButton_pressed():
+	_timer.paused = not _timer.paused
+	if _timer.paused:
+		$AnimationPlayer.play("blink")
+	else:
+		_stop_blink_animation()
+
+		
+func _stop_blink_animation():
+	$AnimationPlayer.seek(0, true)
+	$AnimationPlayer.stop()
